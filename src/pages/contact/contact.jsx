7da4595e7238079affa1d3edb0f8b4be9379750e
Murod-A <murodahmadsoev@gmail.com>
2025-07-17 16:52:45 +0500

    changes

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-
-
+import ContactStats from '../../components/contactState'
+import ContactForm from '../../components/contactForm'
 
 const Contact = () => {
-  return (
-	 <div>
-    <section className='mt-10'>
-            <ContactStats/>
-           </section>
-    
-           <section className='mt-10'>
-            <ContactForm/>
-           </section>
-   </div>
-  )
+	return (
+		<div>
+			<section className='mt-10'>
+				<ContactStats />
+			</section>
+
+			<section className='mt-10'>
+				<ContactForm />
+			</section>
+		</div>
+	)
 }
 
 export default Contact
